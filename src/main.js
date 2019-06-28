61d7f5c5f90c63165278c7c24f9760af6c397504
ElitaGomez <ecgomezr@gmail.com>
2019-06-28 06:04:31 -0500

    diseño del estilo del login

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 /* Manejo del DOM */
+const pokeData = POKEMON.pokemon;
+const pantallaLogin = document.getElementById("login");
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const error = document.getElementById("error");
+const enviar = document.getElementById("enviar");
+const pantallaPagina = document.getElementById("pagina");
+const contenedor = document.getElementById("contenedor");
+
+enviar.addEventListener('click', () => {
+    if (password.value === 'LABORATORIA' && name.value === 'LABORATORIA') {
+        pantallaLogin.classList.toggle("ocultar");
+        pantallaPagina.classList.toggle("ocultar");
+    } else {
+        password.value = "";
+        name.value = "";
+        error.innerHTML = 'contraseña incorrecta';
+    }
+
+});
+
