8989bdac43b40bcaae306cac1b51f26176e81444
ElitaGomez <ecgomezr@gmail.com>
2019-07-07 23:54:17 -0500

    terminando vista css

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 /* Manejo del DOM */
 const pokeData =POKEMON.pokemon;
-const pantallaLogin = document.getElementById("login");
+const pantallaLogin = document.getElementById("cajalogin");
 const name = document.getElementById("name");
 const password = document.getElementById("password");
 const error = document.getElementById("error");
-const enviar = document.getElementById("enviar");
-const pantallaPagina = document.getElementById("pagina");
-const contenedor = document.getElementById("contenedor"); 
-
+const enviar = document.getElementById("btnenviar");
+const pantallaPagina = document.getElementById("pokecaja");
+const contenedor = document.getElementById("contenedor");
+const ordena = document.getElementById("a-z");
+const numero = document.getElementById("numero");
+const tipos = document.getElementById("tipos");
 
 enviar.addEventListener('click',() => {
     
     
-    if( password.value === 'LABORATORIA' && name.value==='LABORATORIA'){
+    if( password.value === '' && name.value===''){
 
         pantallaLogin.classList.toggle("ocultar");
         pantallaPagina.classList.toggle("ocultar");
@@ -25,31 +27,33 @@ enviar.addEventListener('click',() => {
 
 });
 
- const mostrarPoker = (data) => {
+const mostrarPoker = (data) => {
 
-     let mostrar = '';
-
-     for(let i = 0; i< data.length; i++){
-
-         let llamado = `
-         <div class= "verpokemon">
-         <figure class="figure"
+    let mostrar = '';
+    for (let i = 0; i < data.length; i++) {
+        let llamado = `
+         <div class="cajapokemon">
             <img src ="${data[i].img}"/>
             <p>Nombre : ${data[i].name}</p>
+            <P>Numero : ${data[i].num}</p>
+            <p>Tipo  : ${data[i].type}</p>
          </div>`;
-         
-         mostrar+= llamado;
+
+        mostrar += llamado;
     }
     return mostrar;
 
- };
- contenedor.innerHTML = mostrarPoker(pokeData);
+};
+contenedor.innerHTML = mostrarPoker(pokeData);
 
- <div class="show-pokemon">
-     <figure class="figure">
-       <img src="${pokemon[i].img}"/>
-     </figure>
-     <div class="poke-name">
-       <p>${pokemon[i].name}</p>
-     </div>
-   </div>`;
+tipos.addEventListener('change', () => {
+    contenedor.innerHTML = mostrarPoker(window.pokemon.filtrarData(pokeData, tipos.value));
+});
+
+ordena.addEventListener('change', () => {
+    contenedor.innerHTML = mostrarPoker(window.pokemon.ordenaPoke(pokeData, ordena.value));
+});
+
+numero.addEventListener('change', () => {
+    contenedor.innerHTML = mostrarPoker(window.pokemon.numeroPoke(pokeData, numero.value));
+});
